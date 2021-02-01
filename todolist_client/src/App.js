b607815aacbd220a34683380bf0b9fa3e4dd3115
Sanghyuk Choi <nugu1223@gmail.com>
2021-02-01 17:17:30 +0900

    eslint check

--- todolist_client/src/App.js
+++ todolist_client/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log('app')
+  console.log('eslintcheck')
   return (
     <div className="App">
       <header className="App-header">
