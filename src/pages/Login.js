11e585be0b8d09b5dc0b633e8337b2ad2e50e8d5
danielsychoo <choointern@gmail.com>
2021-02-03 12:04:04 +0900

    [01] Update Login page leftSide
    Login.js를 통해 Login page의 leftSide를 구축.
    Login.css를 통해 leftSide 기초 CSS 설정.
    a tag에 link 연결 주소가 들어가지 않음으로
    esLint에서 error로 잡기에
    a tag가 error로 잡히지 않도록
    .eslintrc.js에 rules 추가

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import '../css/Login.css';
 
 class Login extends React.Component {
   render() {
     return (
-      <div>
-        <div className="leftSide">leftSide</div>
+      <div className="allLoginPage">
+        <div className="leftSide">
+          <div className="leftMiddle">
+            <div className="logo">Logo</div>
+            <div className="comment">
+              <div className="mainName">TODO LIST</div>
+              <div className="mainText">
+                <div>WHAT IF YOU HAVEN'T</div>
+                <div>SIGNED UP YET?</div>
+              </div>
+              <div className="lastWord">
+                <span>Not a member?</span>
+                <a href="#">
+                  <span>Sign up now</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="rightSide">rightSide</div>
       </div>
     );
