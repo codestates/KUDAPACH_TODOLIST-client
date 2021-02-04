37faf3e48196cbd7c1ea2716b65a62c07e5f8ca8
danielsychoo <choointern@gmail.com>
2021-02-04 21:14:47 +0900

    [task22] Add all validation in Signup.js
    각 validation function 사용하여 Signup.js 구축

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../css/Signup.css';
 import { withRouter, Link } from 'react-router-dom';
+import swal from 'sweetalert';
+import { emailValidation, phoneNumValidation } from '../pages/ValidationFun';
 
 class Signup extends React.Component {
   constructor(props) {
@@ -12,10 +14,60 @@ class Signup extends React.Component {
       phoneNumber: '',
     };
     this.handleNewInput = this.handleNewInput.bind(this);
+    this.handleSignup = this.handleSignup.bind(this);
   }
   // input value를 가져오는 함수
   handleNewInput = (key) => (e) => {
     this.setState({ [key]: e.target.value });
+  };
+  // SignupBtn 실행 함수
+  handleSignup = () => {
+    if (
+      !this.state.email ||
+      !this.state.password ||
+      !this.state.userName ||
+      !this.state.phoneNumber
+    ) {
+      swal({
+        title: 'Something is empty',
+        text: 'All things have to exist',
+        icon: 'warning',
+        button: 'confirm',
+      });
+    } else {
+      if (emailValidation(this.state.email) === false) {
+        swal({
+          title: 'Wrong email',
+          text: 'Check your email',
+          icon: 'warning',
+          button: 'confirm',
+        });
+      }
+      if (this.state.password.length < 8) {
+        swal({
+          title: 'Wrong password',
+          text: 'password need atleast 8 words',
+          icon: 'warning',
+          button: 'confirm',
+        });
+      }
+      if (!this.state.userName) {
+        swal({
+          title: 'Username is empty',
+          text: 'Please input username',
+          icon: 'warning',
+          button: 'confirm',
+        });
+      }
+      if (phoneNumValidation(this.state.phoneNumber) === false) {
+        swal({
+          title: 'Wrong phone number',
+          text: 'Input just numbers',
+          icon: 'warning',
+          button: 'confirm',
+        });
+      }
+    }
   };
 
   render() {
@@ -47,7 +99,7 @@ class Signup extends React.Component {
                 onChange={this.handleNewInput('email')}
               />
               <input
-                type="text"
+                type="password"
                 className="newPassword"
                 placeholder="Password"
                 onChange={this.handleNewInput('password')}
@@ -64,7 +116,9 @@ class Signup extends React.Component {
                 placeholder="Phone Number"
                 onChange={this.handleNewInput('phoneNumber')}
               />
-              <button className="createBtn">CREATE ACCOUNT</button>
+              <button className="createBtn" onClick={this.handleSignup}>
+                CREATE ACCOUNT
+              </button>
             </div>
           </div>
         </div>
--- src/pages/ValidationFun.js
+++ src/pages/ValidationFun.js
@@ -5,19 +5,12 @@ const emailValidation = (newEmail) => {
   if (emailExp.test(newEmail)) return true;
   else return false;
 };
-// password가 8~20자리인지 체크하는 정규표현식 함수
-const passwordValidation = (newPassword) => {
-  let passwordExp = /^{8,20}$/;
-
-  if (passwordExp.test(newPassword)) return true;
-  else return false;
-};
 // phoneNum이 숫자만, 10,11자리인지 체크하는 정규표현식 함수
 const phoneNumValidation = (newPhone) => {
-  let phoneExp = /^01([0|1|6|7|8|9]?)?([0-9]{3,4})?([0-9]{4})$/;
+  let phoneExp = /^\d{3}\d{3,4}\d{4}$/;
 
   if (phoneExp.test(newPhone)) return true;
   else return false;
 };
 
-export { emailValidation, passwordValidation, phoneNumValidation };
+export { emailValidation, phoneNumValidation };
