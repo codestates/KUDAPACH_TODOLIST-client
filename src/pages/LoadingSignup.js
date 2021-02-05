2e3273a9a59405dd7e0789316c30192914228b48
danielsychoo <choointern@gmail.com>
2021-02-05 18:28:44 +0900

    [task10] LoadingSignup page complete
    추후 서버와 연결시 axios만 연결시키면 끝

--- src/pages/LoadingSignup.js
+++ src/pages/LoadingSignup.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import '../css/LoadingSignup.css';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter, Link, Route, Switch, Redirect } from 'react-router-dom';
+import Signup from './Signup';
 
 class LoadingSignup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 5,
+    };
+    this.counter = this.counter.bind(this);
+    this.timer = this.timer.bind(this);
+  }
+  // 첫 rendering 후 실행
+  componentDidMount() {
+    this.counter();
+  }
+  // state update 후 실행
+  componentDidUpdate() {
+    this.counter();
+  }
+  // 1초마다 callback을 실행시키는 함수
+  counter = () => setTimeout(this.timer, 1000);
+  // this.state.count를 -1하는 함수
+  timer = () => {
+    this.setState({ count: this.state.count - 1 });
+    if (this.state.count < 0) {
+      this.timer();
+    }
+  };
+
   render() {
+    const { count } = this.state;
     return (
       <div className="allLoginPage">
         <div className="loadingLeftSide">
@@ -27,10 +55,21 @@ class LoadingSignup extends React.Component {
               Thank you for signing up!
             </div>
             <div className="boldLine">
-              Redirecting to Sign In page 5 seconds.
+              Redirecting to Sign In page {count} seconds.
             </div>
             <div className="notBoldLine">Please check your email</div>
           </div>
+          <Switch>
+            <Route path="/Signup" render={() => <Signup />} />
+            <Route
+              path="/"
+              render={() => {
+                if (count === 0) {
+                  return <Redirect to="/Signup" />;
+                }
+              }}
+            />
+          </Switch>
         </div>
       </div>
     );
