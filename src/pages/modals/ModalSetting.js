7ef516e1b3ccb457e11ab5c2e92af36458c2f17b
danielsychoo <choointern@gmail.com>
2021-02-08 20:25:10 +0900

    [task13] Add Link to Mypage.js
    세팅에서 Mypage를 누르면 Mypage로 넘어가게 설정

--- src/pages/modals/ModalSetting.js
+++ src/pages/modals/ModalSetting.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../css/ModalSetting.css';
+import { Link } from 'react-router-dom';
 
 class ModalSetting extends React.Component {
   render() {
@@ -8,14 +9,16 @@ class ModalSetting extends React.Component {
         <div className="accountText">Account</div>
         <div className="hrBox"></div>
         <div className="userBox">
-          <div className="userLogo" src="user.svg" alt="" />
+          <div className="userLogo" src="user.svg" alt="user logo img" />
           <div className="userInfo">
             <div className="userName">kudapach</div>
             <div className="userEmail">kudapach@gmail.com</div>
           </div>
         </div>
         <div className="hrBox"></div>
-        <div className="mypagesetText">My Page</div>
+        <Link to="/mypage" className="mypagesetText">
+          <div>My Page</div>
+        </Link>
         <div className="groupsetText">Group Setting</div>
         <div className="signoutText">Sign Out</div>
       </div>
