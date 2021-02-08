7bd7dca828ee47ee9f0689256b06e57a0ef81199
danielsychoo <choointern@gmail.com>
2021-02-08 21:42:39 +0900

    [task12] Add React-Calendar in SingleUserNav

--- src/pages/SingleUserNav.js
+++ src/pages/SingleUserNav.js
@@ -1,21 +1,33 @@
-/*eslint-disable*/
-
 import React, { useState } from 'react';
 import '../css/SingleUserNav.css';
 import ModalSetting from './modals/ModalSetting';
+import ModalCalendar from 'react-calendar';
+import '../css/Calendar.css';
 
 function SingleUserNav() {
   // setting 모달창에 대한 state hook과 function들 ---- 시작
   const [settingModal, setSettingModal] = useState('false');
   const toggleModalSetting = () => {
-    if(settingModal === 'false') {
+    if (settingModal === 'false') {
       setSettingModal('true');
     }
-    if(settingModal === 'true') {
+    if (settingModal === 'true') {
       setSettingModal('false');
     }
   };
   // setting 모달창에 대한 state hook과 function들 ---- 끝
+
+  // calendar 모달창에 대한 state hook과 function들 ---- 시작
+  const [calendarModal, setCalendarModal] = useState('false');
+  const toggleCalendarModal = () => {
+    if (calendarModal === 'false') {
+      setCalendarModal('true');
+    }
+    if (calendarModal === 'true') {
+      setCalendarModal('false');
+    }
+  };
+  // calendar 모달창에 대한 state hook과 function들 ---- 끝
 
   return (
     <div className="body__container">
@@ -33,14 +45,22 @@ function SingleUserNav() {
 
           <div className="sign-group float--right">
             <div className="btn-group ">
-              <div className="Search_Calendar_btn">Search Calendar</div>
+              <div
+                className="Search_Calendar_btn"
+                onClick={toggleCalendarModal}
+              >
+                Search Calendar
+              </div>
               <div className="user"> </div>
-              <div className="setting" onClick={toggleModalSetting}> </div>
+              <div className="setting" onClick={toggleModalSetting}>
+                {' '}
+              </div>
             </div>
           </div>
         </div>
       </header>
       {settingModal === 'true' ? <ModalSetting /> : <div />}
+      {calendarModal === 'true' ? <ModalCalendar /> : <div />}
     </div>
   );
 }
