2cbb020b7a2b0a47a4535fc4d04ae994345024c8
danielsychoo <choointern@gmail.com>
2021-02-08 17:04:34 +0900

    [task25] Update mypage.js validation check

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,24 +1,97 @@
+// /*eslint-disable*/
+
 import React from 'react';
 import SingleUserNav from './SingleUserNav';
 import '../css/Mypage.css';
+import { phoneNumValidation } from '../pages/ValidationFun';
+import swal from 'sweetalert';
 
 class Mypage extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      useremail: 'test@test.com',
+      email: 'test@test.com',
       username: 'kudapach',
-      usermobile: '01012345678',
-      // 추후 RDS에서 가져온 DATA로 state설정
+      mobile: '01012345678',
+      prevPassword: '11111111',
+      currentPassword: '',
+      password: '',
+      passwordConfirm: '',
+      // 추후 RDS에서 가져온 DATA로 설정
     };
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+    this.handleSaveButton = this.handleSaveButton.bind(this);
+    this.checkChangedInfo = this.checkChangedInfo.bind(this);
   }
 
-  // handleSaveButton() {
-  //
-  // }
+  handleChangeInput = (key) => (e) => {
+    this.setState({ [key]: e.target.value });
+  };
+
+  checkChangedInfo() {
+    const {
+      username,
+      mobile,
+      prevPassword,
+      currentPassword,
+      password,
+      passwordConfirm,
+    } = this.state;
+    // 비밀번호를 변경한 경우 true
+    if (
+      username &&
+      mobile &&
+      password &&
+      currentPassword === prevPassword &&
+      password === passwordConfirm
+    ) {
+      return true;
+    }
+    // username, mobile만 변경한 경우 true
+    if (
+      username &&
+      mobile &&
+      !currentPassword &&
+      !password &&
+      !passwordConfirm
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  handleSaveButton() {
+    if (this.checkChangedInfo() === false) {
+      swal({
+        title: 'Some Information is wrong',
+        text: 'Please check your Information',
+        icon: 'warning',
+        button: 'confirm',
+      });
+    }
+    if (this.checkChangedInfo() === true) {
+      // console.log(this.state);
+      swal({
+        title: 'Cool!',
+        text: 'Information is changed',
+        icon: 'success',
+        button: 'confirm',
+      });
+    }
+  }
 
   render() {
-    const { useremail, username, usermobile } = this.state;
+    const {
+      email,
+      username,
+      mobile,
+      prevPassword,
+      currentPassword,
+      password,
+      passwordConfirm,
+    } = this.state;
 
     return (
       <div>
@@ -27,16 +100,80 @@ class Mypage extends React.Component {
           <div className="mypageMainWrapper">
             <div className="userInfoWrapper">
               <div className="mypageText">MY PAGE</div>
-              <div className="useremailText">{useremail}</div>
-              <input type="text" value={username} />
-              <input type="text" value={usermobile} />
+              <div className="useremailText">{email}</div>
+              <input
+                type="text"
+                className={
+                  username.length === 0
+                    ? ''
+                    : !username
+                    ? 'tryAgain'
+                    : 'allGood'
+                }
+                value={username}
+                onChange={this.handleChangeInput('username')}
+              />
+              <input
+                type="text"
+                className={
+                  mobile.length === 0
+                    ? ''
+                    : !phoneNumValidation(mobile)
+                    ? 'tryAgain'
+                    : 'allGood'
+                }
+                value={mobile}
+                onChange={this.handleChangeInput('mobile')}
+              />
             </div>
             <div className="changePasswordWrapper">
               <div className="mypageText">CHANGE PASSWORD</div>
-              <input type="password" placeholder="Current Password" />
-              <input type="password" placeholder="New Password" />
-              <input type="password" placeholder="Confirm Password" />
-              <button className="infoConfirm">SAVE</button>
+              <input
+                type="password"
+                className={
+                  currentPassword.length === 0
+                    ? ''
+                    : prevPassword !== currentPassword
+                    ? 'tryAgain'
+                    : 'allGood'
+                }
+                placeholder="Current Password"
+                onChange={this.handleChangeInput('currentPassword')}
+              />
+              <input
+                type="password"
+                className={
+                  password.length === 0
+                    ? ''
+                    : password.length < 8
+                    ? 'tryAgain'
+                    : 'allGood'
+                }
+                placeholder="New Password"
+                onChange={this.handleChangeInput('password')}
+              />
+              <input
+                type="password"
+                className={
+                  passwordConfirm.length === 0
+                    ? ''
+                    : password !== passwordConfirm
+                    ? 'tryAgain'
+                    : 'allGood'
+                }
+                placeholder="Confirm Password"
+                onChange={this.handleChangeInput('passwordConfirm')}
+              />
+              <button
+                className={
+                  this.checkChangedInfo() === true
+                    ? 'infoConfirm'
+                    : 'notConfirm'
+                }
+                onClick={this.handleSaveButton}
+              >
+                SAVE
+              </button>
             </div>
           </div>
         </div>
