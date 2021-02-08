3f766ce065647ebff2a5e6f1b5e065cc2b271c85
danielsychoo <choointern@gmail.com>
2021-02-08 20:10:07 +0900

    [task13] Update ModalSetting.js & css
    ModalSetting.js와 css를 통해 필요한 디자인을
    모두 구축함.

--- src/pages/modals/ModalSetting.js
+++ src/pages/modals/ModalSetting.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import '../../css/ModalSetting.css';
 
 class ModalSetting extends React.Component {
   render() {
-    return <div>this is modal!!!!@#!@$!@#!@#SETTING!!@#$!@%!@#</div>;
+    return (
+      <div className="settingWrapper">
+        <div className="accountText">Account</div>
+        <div className="hrBox"></div>
+        <div className="userBox">
+          <div className="userLogo" src="user.svg" alt="" />
+          <div className="userInfo">
+            <div className="userName">kudapach</div>
+            <div className="userEmail">kudapach@gmail.com</div>
+          </div>
+        </div>
+        <div className="hrBox"></div>
+        <div className="mypagesetText">My Page</div>
+        <div className="groupsetText">Group Setting</div>
+        <div className="signoutText">Sign Out</div>
+      </div>
+    );
   }
 }
 
