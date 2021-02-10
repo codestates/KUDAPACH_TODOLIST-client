b0cff133bce0eee8f6c8644f59b84c777bbc0803
danielsychoo <choointern@gmail.com>
2021-02-10 23:11:00 +0900

    [task29] Update ModalGroup.js whichGroup state
    그룹 modal창에서 그룹 클릭시 해당 그룹의 groupid가
    state인 whichGroup으로 set되도록 설정.
    추후 이 state의 값이 id인 group의 todolist data를
    가져오기 위한 작업

--- src/pages/modals/ModalGroup.js
+++ src/pages/modals/ModalGroup.js
@@ -5,31 +5,42 @@ class ModalGroup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isGroup: 'none',
+      whichGroup: 0,
     };
     this.selectGroupHandler = this.selectGroupHandler.bind(this);
   }
 
   selectGroupHandler(e) {
-    this.setState({ isGroup: e.target.getAttribute('value') });
+    this.setState({ whichGroup: e.target.getAttribute('value') });
   }
 
   render() {
     const { groupinfo } = this.props;
+    // groupid와 name이 각 다른 객체에 있어 map으로 component를 출력하기 힘드므로
+    // 2중배열로 다시금 만들어냄 (추후 리팩토링 가능시 부탁드립니다. 저는 이 이상은..ㅜ)
+    let groupIdNamesInfo = [];
+    for (let i = 0; i < groupinfo.groups.length; i++) {
+      let temp = [];
+      temp.push(groupinfo.groups[i].groupid);
+      temp.push(groupinfo.groupnames[i].groupname);
+      groupIdNamesInfo.push(temp);
+      temp = [];
+    }
+    // console.log(groupIdNamesInfo); // 2중배열로 만들어진 것 확인
     return (
       <div className="GroupWrapper">
         <div className="accountText">Group</div>
         <div className="signoutText">My todo</div>
         <div className="hrBox"></div>
-        {groupinfo.groupnames.map((el, id) => {
+        {groupIdNamesInfo.map((el) => {
           return (
             <div
               className="signoutText"
-              key={id}
-              value={el.groupname}
+              key={el[0]}
+              value={el[0]}
               onClick={(e) => this.selectGroupHandler(e)}
             >
-              {el.groupname}
+              {el[1]}
             </div>
           );
         })}
