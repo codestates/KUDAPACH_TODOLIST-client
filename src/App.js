54e857dc11c635ba202eca84224aa89262330c41
Sanghyuk Choi <nugu1223@gmail.com>
2021-02-11 18:37:16 +0900

    test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isSignin: false,
+      isSignin: true,
       userinfo: null,
       groupinfo: null,
       isGuest: false,
